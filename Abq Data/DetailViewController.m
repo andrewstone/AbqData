06e12a069b57aa73b19b9b1e62b13eca4818c279
Andrew Stone <andrew@stone.com>
2015-01-07 16:04:14 -0700

    Readme, cleaned up DetailVC

--- Abq Data/DetailViewController.m
+++ Abq Data/DetailViewController.m
@@ -298,6 +298,7 @@ static NSNumberFormatter *numberFormatter = nil;
 	__block NSMutableData *data;
 	
 	// 1. save inData to a zipFile - it may contain multiple files
+	NSLog(folder);
 	
 	NSString *filePath = [folder stringByAppendingPathComponent:kmz];
 	if ([inData writeToFile:filePath options:NSDataWritingAtomic error:&error]) {
@@ -342,125 +343,70 @@ static NSNumberFormatter *numberFormatter = nil;
 }
 
 - (void)decompressAndLoadKMZ:(NSData *)data {
-	// create /tmp dir
+
 	NSString *folder = [self nextUniqueTempFolder];
-	
-    // decompress the data and create new kmlData object
-	// this is a fail for now:
-//	NSData *kmlData = [self uncompressGZip:data];
-//	NSString *kml = [[DataEngine dataEngine]stringForData:kmlData];
-	
 	NSString *kml = [self kmlContents:data resourceFolder:folder];
 	
 	KMLViewerViewController *kvc = [[KMLViewerViewController alloc]initWithKML:kml resourceFolder:folder];
-    NSLog(@"kmlData dump: %@", kml);
-	
+NSLog(folder);
 	// so it doesn't get reloaded on NEXT viewDidLoad!
 	self.objects = @[data];
 	
 	[self.navigationController pushViewController:kvc animated:YES];
-
-	
-	
-    // Chris' notes/todos:  I'm basing this on Andrew's process below, but not using a file based approach.
-    // TODO: enumerate objects in the kml file.  This is where using a lib would be useful
-    // TODO: invoke the KMLParser
-    
-    // Andrew's notes:
-    // I was going to get KML from KMZ and then load KMLViewerViewController
-    // save to local file then something like:
-    /*
-     if (filePath) {
-     __block NSMutableData *data;
-     ZipFile *kmzFile;
-     @try {
-     kmzFile = [[ZipFile alloc] initWithFileName:filePath mode:ZipFileModeUnzip];
-     
-     [kmzFile.listFileInZipInfos enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop)
-     {
-     FileInZipInfo *info = (FileInZipInfo *)obj;
-     
-     NSString *ext = info.name.pathExtension.lowercaseString;
-     
-     if ([ext isEqualToString:@"kml"]) {
-     [kmzFile locateFileInZip:info.name];
-     
-     ZipReadStream *reader= kmzFile.readCurrentFileInZip;
-     data = [[NSMutableData alloc] initWithLength:info.length];
-     [reader readDataWithBuffer:data];
-     [reader finishedReading];
-     
-     *stop = YES;
-     }
-     }];
-     }
-     @catch (NSException *exception) {
-     DLog(@"exception, %@", [exception debugDescription]);
-     }
-     @finally {
-     if (kmzFile) {
-     [kmzFile close];
-     }
-     }
-     
-     if (data) {
-     self.kml = [KMLParser parseKMLWithData:data];
-     }
-     
-     }
-     */
 }
 
-// Method to decompress GZip data from: http://stackoverflow.com/questions/8425012/is-there-a-practical-way-to-compress-nsdata
-- (NSData *)uncompressGZip:(NSData *)compressedData {
-    
-    if ([compressedData length] == 0)
-        return compressedData;
-    
-    NSUInteger full_length = [compressedData length];
-    NSUInteger half_length = [compressedData length] / 2;
-    
-    NSMutableData *decompressed = [NSMutableData dataWithLength: full_length + half_length];
-    BOOL done = NO;
-    int status;
-    
-    z_stream strm;
-    strm.next_in = (Bytef *)[compressedData bytes];
-    strm.avail_in = (unsigned int)[compressedData length];
-    strm.total_out = 0;
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    
-    if (inflateInit2(&strm, (15+32)) != Z_OK)
-        return nil;
-    
-    while (!done) {
-        // Make sure we have enough room and reset the lengths.
-        if (strm.total_out >= [decompressed length]) {
-            [decompressed increaseLengthBy: half_length];
-        }
-        strm.next_out = [decompressed mutableBytes] + strm.total_out;
-        strm.avail_out = (unsigned int)([decompressed length] - strm.total_out);
-        
-        // Inflate another chunk.
-        status = inflate (&strm, Z_SYNC_FLUSH);
-        if (status == Z_STREAM_END) {
-            done = YES;
-        } else if (status != Z_OK) {
-            break;
-        }
-    }
-    if (inflateEnd (&strm) != Z_OK)
-        return nil;
-    
-    // Set real length.
-    if (done) {
-        [decompressed setLength: strm.total_out];
-        return [NSData dataWithData: decompressed];
-    } else {
-        return nil;
-    }
-}
+// The reason this won't work is because KMZ can contain multiple files
+
+//// Method to decompress GZip data from: http://stackoverflow.com/questions/8425012/is-there-a-practical-way-to-compress-nsdata
+//- (NSData *)uncompressGZip:(NSData *)compressedData {
+//    
+//    if ([compressedData length] == 0)
+//        return compressedData;
+//    
+//    NSUInteger full_length = [compressedData length];
+//    NSUInteger half_length = [compressedData length] / 2;
+//    
+//    NSMutableData *decompressed = [NSMutableData dataWithLength: full_length + half_length];
+//    BOOL done = NO;
+//    int status;
+//    
+//    z_stream strm;
+//    strm.next_in = (Bytef *)[compressedData bytes];
+//    strm.avail_in = (unsigned int)[compressedData length];
+//    strm.total_out = 0;
+//    strm.zalloc = Z_NULL;
+//    strm.zfree = Z_NULL;
+//    
+//    if (inflateInit2(&strm, (15+32)) != Z_OK)
+//        return nil;
+//    
+//    while (!done) {
+//        // Make sure we have enough room and reset the lengths.
+//        if (strm.total_out >= [decompressed length]) {
+//            [decompressed increaseLengthBy: half_length];
+//        }
+//        strm.next_out = [decompressed mutableBytes] + strm.total_out;
+//        strm.avail_out = (unsigned int)([decompressed length] - strm.total_out);
+//        
+//        // Inflate another chunk.
+//        status = inflate (&strm, Z_SYNC_FLUSH);
+//        if (status == Z_STREAM_END) {
+//            done = YES;
+//        } else if (status != Z_OK) {
+//            break;
+//        }
+//    }
+//    if (inflateEnd (&strm) != Z_OK)
+//        return nil;
+//    
+//    // Set real length.
+//    if (done) {
+//        [decompressed setLength: strm.total_out];
+//        return [NSData dataWithData: decompressed];
+//    } else {
+//        return nil;
+//    }
+//}
 
 
 #pragma mark - Selector handlers
--- README.md
+++ README.md
@@ -1,23 +1,21 @@
 AbqData   
 =======
 
-Tools to explore ABQ Open Data Initiative city gov data. http://www.cabq.gov/abq-data/abq-data#data-sets
+Tools to explore ABQ Open Data Initiative city gov data. 
+
+http://www.cabq.gov/abq-data/abq-data#data-sets
 
 Tasks - search for '// TODO' 
 =====
 1. go through http://www.cabq.gov/abq-data/abq-data#data-sets and build out AbqData.json 
 
-
 2. For each dataset, test and enhance how you display the results
 
-3. Datasets untouched: KML (google map)
+3. Now we can load KMZ & KML - work on maps where we have multiple lines from the geometry payload, not just a point.
 
 
-Questions for ABQData:
-
+Questions for ABQData
+=====================
 1. why <HEAD> and not refresh meta tag?
-2. Public Art: why Flickr? It has no direct loading and it hits google, yahoo, etc. This means CABQ is spying on it's users geo.yahoo.com 443, y.analytics.yahoo.com 443
-
-
-
-'
+2. Public Art: why Flickr? It has no direct loading and it hits google, yahoo, etc
+3. Unified API (the shim layer) - the city might want to provide this or SWCP can
